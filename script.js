522ab6beff8af4c7d74ca5aa4765b4c482d09eaa
Vadim <shadrinv686@gmail.com>
2021-10-25 19:38:28 +0600

    Add initial version function game()

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-console.log("Hello world!");
-
+const INVITATION = 'Let\'s play with you in the game!';
+const MAKE_CHOISE = "Make your choise, rock paper or scissors?";
+// function return random computer's answer
+console.log(INVITATION);
+console.log(MAKE_CHOISE);
 let userAnswer;
+let computerAnswer = '';
 function computerPlay () {
     let computerChoise = getRandomNumber(1, 3);
     if (computerChoise == 1) {
-        console.log("Rock");
+        computerAnswer += "Rock";
     } else if (computerChoise == 2) {
-        console.log("Paper");
+        computerAnswer += "Paper";
     } else {
-        console.log("Scissors")
+        computerAnswer += "Scissors";
     }
+    console.log(computerAnswer);
+    return computerAnswer;
     
 }
 
@@ -17,4 +23,24 @@ function getRandomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(computerPlay());
+computerPlay();
+
+
+
+function playerSelection () {
+    userAnswer = prompt ('Input your answer', '');
+    console.log(userAnswer);
+    return userAnswer;
+    
+}
+playerSelection();
+
+function game() {
+    if (userAnswer == 'Rock' && computerAnswer == 'Rock') {
+        console.log ("Gz you win!");
+    } else if (userAnswer == 'Rock' && computerAnswer == 'Paper') {
+        console.log('You lose!');
+    }
+    
+}
+game();
