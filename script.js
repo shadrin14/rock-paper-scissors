24f2ac748d974a9e1dd8bcfd4644bd95c907cb4d
Vadim <shadrinv686@gmail.com>
2021-10-25 18:35:18 +0600

    Added random number function getRandomNumber

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+function computerPlay () {
+
+}
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRandomNumber(1, 3));
