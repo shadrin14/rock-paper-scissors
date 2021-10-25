d4878999ededfd313e925774f772f6430bda17db
Vadim <shadrinv686@gmail.com>
2021-10-25 18:49:25 +0600

    The function computerPlay returns the computer's coise

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 console.log("Hello world!");
-function computerPlay () {
 
+let userAnswer;
+function computerPlay () {
+    let computerChoise = getRandomNumber(1, 3);
+    if (computerChoise == 1) {
+        console.log("Rock");
+    } else if (computerChoise == 2) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors")
+    }
+    
 }
+
 function getRandomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandomNumber(1, 3));
+
+console.log(computerPlay());
