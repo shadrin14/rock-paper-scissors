f380b5c7ea240241a7b0ec6837585d48e137a888
Vadim <shadrinv686@gmail.com>
2021-11-05 19:55:11 +0600

    Get buttons by selector

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let choose = 0;
-
-
     
+    
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOptions = [rockBtn, paperBtn, scissorBtn];
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        
+    }
 }
